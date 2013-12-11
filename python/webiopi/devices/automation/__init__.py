4fbae9315dad9ff7b8bbfadf4cb19561f4f8b7d9
hesperus22 <przemko@przemko-robakowski.pl>
2013-12-11 23:51:53 +0100

    Fixed indentation

--- python/webiopi/devices/automation/__init__.py
+++ python/webiopi/devices/automation/__init__.py
@@ -16,78 +16,78 @@ from webiopi import GPIO, deviceInstance
 from webiopi.protocols.rest import *
 
 class Roller():
-	def __init__(self, upPin, downPin, aPin, bPin, upPort=GPIO, downPort=GPIO, aPort=GPIO, bPort=GPIO):
-		self.upPort = GPIO
-		if upPort!=GPIO :
-			self.upPort = deviceInstance(upPort)
-		self.upPin = upPin
-		self.upPort.setFunction(upPin, GPIO.IN, GPIO.PUD_UP)
-			
-		self.downPort = GPIO
-		if downPort!=GPIO :
-			self.downPort = deviceInstance(downPort)
-		self.downPin = downPin
-		self.downPort.setFunction(downPin, GPIO.IN, GPIO.PUD_UP)
-			
-		self.aPort = GPIO
-		if aPort!=GPIO :
-			self.aPort = deviceInstance(aPort)
-		self.aPin = aPin
-		self.aPort.setFunction(aPin, GPIO.OUT)
-			
-		self.bPort = GPIO
-		if bPort!=GPIO :
-			self.bPort = deviceInstance(bPort)
-		self.bPin = bPin
-		self.bPort.setFunction(bPin, GPIO.OUT)
-		
-	def __family__(self):
+    def __init__(self, upPin, downPin, aPin, bPin, upPort=GPIO, downPort=GPIO, aPort=GPIO, bPort=GPIO):
+        self.upPort = GPIO
+        if upPort!=GPIO :
+            self.upPort = deviceInstance(upPort)
+        self.upPin = upPin
+        self.upPort.setFunction(upPin, GPIO.IN, GPIO.PUD_UP)
+            
+        self.downPort = GPIO
+        if downPort!=GPIO :
+            self.downPort = deviceInstance(downPort)
+        self.downPin = downPin
+        self.downPort.setFunction(downPin, GPIO.IN, GPIO.PUD_UP)
+            
+        self.aPort = GPIO
+        if aPort!=GPIO :
+            self.aPort = deviceInstance(aPort)
+        self.aPin = aPin
+        self.aPort.setFunction(aPin, GPIO.OUT)
+            
+        self.bPort = GPIO
+        if bPort!=GPIO :
+            self.bPort = deviceInstance(bPort)
+        self.bPin = bPin
+        self.bPort.setFunction(bPin, GPIO.OUT)
+        
+    def __family__(self):
         return "Roller"
-		
-	@request("POST", "up")
-	def up(self):
-		if self.isUp():
-			return
-		
-		self.aPort.digitalWrite(self.aPin, GPIO.HIGH)
-		self.bPort.digitalWrite(self.bPin, GPIO.LOW)
-		
-		while not self.isUp():
-			pass
-		
-		self.aPort.digitalWrite(self.aPin, GPIO.LOW)
-		
-	
-	@request("POST", "down")
-	def down(self):
-		if self.isDown():
-			return
-		
-		self.aPort.digitalWrite(self.aPin, GPIO.LOW)
-		self.bPort.digitalWrite(self.bPin, GPIO.HIGH)
-		
-		while not self.isDown():
-			pass
-		
-		self.bPort.digitalWrite(self.bPin, GPIO.LOW)
-	
-	@request("GET", "state")
-	@response("%s")
-	def state(self):
-		if self.isUp():
-			return "Up"
-		
-		if self.isDown():
-			return "Down"
-			
-		if self.aPort.digitalRead(self.aPin) == GPIO.HIGH:
-			return "Going up"
-		
-		if self.bPort.digitalRead(self.bPin) == GPIO.HIGH:
-			return "Going down"
-			
-	def isUp(self):
-		return self.upPort.digitalRead(self.upPin) == GPIO.LOW
-		
-	def isDown(self):
-		return self.downPort.digitalRead(self.downPin) == GPIO.LOW
+        
+    @request("POST", "up")
+    def up(self):
+        if self.isUp():
+            return
+        
+        self.aPort.digitalWrite(self.aPin, GPIO.HIGH)
+        self.bPort.digitalWrite(self.bPin, GPIO.LOW)
+        
+        while not self.isUp():
+            pass
+        
+        self.aPort.digitalWrite(self.aPin, GPIO.LOW)
+        
+    
+    @request("POST", "down")
+    def down(self):
+        if self.isDown():
+            return
+        
+        self.aPort.digitalWrite(self.aPin, GPIO.LOW)
+        self.bPort.digitalWrite(self.bPin, GPIO.HIGH)
+        
+        while not self.isDown():
+            pass
+        
+        self.bPort.digitalWrite(self.bPin, GPIO.LOW)
+    
+    @request("GET", "state")
+    @response("%s")
+    def state(self):
+        if self.isUp():
+            return "Up"
+        
+        if self.isDown():
+            return "Down"
+            
+        if self.aPort.digitalRead(self.aPin) == GPIO.HIGH:
+            return "Going up"
+        
+        if self.bPort.digitalRead(self.bPin) == GPIO.HIGH:
+            return "Going down"
+            
+    def isUp(self):
+        return self.upPort.digitalRead(self.upPin) == GPIO.LOW
+        
+    def isDown(self):
+        return self.downPort.digitalRead(self.downPin) == GPIO.LOW
